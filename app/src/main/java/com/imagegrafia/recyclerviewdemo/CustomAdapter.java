da93bc2bb66a4e91523cce26ccce1330647709a2
ashish <ashishkvs@gmail.com>
2018-03-01 15:15:10 +0530

    cardview on click listener added

--- app/src/main/java/com/imagegrafia/recyclerviewdemo/CustomAdapter.java
+++ app/src/main/java/com/imagegrafia/recyclerviewdemo/CustomAdapter.java
@@ -2,13 +2,16 @@ package com.imagegrafia.recyclerviewdemo;
 
 import android.content.Context;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import java.util.List;
@@ -43,10 +46,16 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ProductVie
 
     @Override
     public void onBindViewHolder(ProductViewHolder holder, int position) {
-        Product product = productList.get(position);
+        final Product product = productList.get(position);
         holder.productName.setText(product.getName());
         holder.productPrice.setText(String.valueOf(product.getPrice()));
         holder.productImage.setImageResource(Integer.parseInt(String.valueOf(product.getImage())));
+        holder.constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"You clicked "+product.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
@@ -61,12 +70,14 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ProductVie
         private TextView productName;
         private TextView productPrice;
         private ImageView productImage;
+        private ConstraintLayout constraintLayout;
 
         public ProductViewHolder(View itemView) {
             super(itemView);
             productName = itemView.findViewById(R.id.productName);
             productPrice = itemView.findViewById(R.id.productPrice);
             productImage = itemView.findViewById(R.id.productImage);
+            constraintLayout=itemView.findViewById(R.id.constraint_layout);
 
         }
     }
